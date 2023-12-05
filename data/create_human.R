04fd51cb734ab6ff32a6576f2ab4fe3f222484ee
Roni Koitermaa <roni.koitermaa@helsinki.fi>
2023-12-05 04:16:59 +0200

    Week 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,78 +1,57 @@
-# Roni Koitermaa 24.11.2023
-# Create combined human development and gender inequality data set.
+# Roni Koitermaa 3.12.2023
+# Raw data set created using create_human_raw.R 
+# (https://github.com/Roninkoi/IODS-project/blob/master/data/create_human_raw.R)
+# Create human data set for week 5 using combined human development and 
+# gender inequality data set.
 
-library(readr)
+# create raw data set
+source("./data/create_human_raw.R")
+# read this data
+human <- read_csv("./data/human.csv")
 
-# https://hdr.undp.org/data-center/human-development-index#/indicies/HDI
-# https://hdr.undp.org/system/files/documents/technical-notes-calculating-human-development-indices.pdf
-hd <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/human_development.csv")
-gii <- read_csv("https://raw.githubusercontent.com/KimmoVehkalahti/Helsinki-Open-Data-Science/master/datasets/gender_inequality.csv", na = "..")
+# Explore dimensions and structure of the human data set.
+# This data set consists of 195 rows, 19 variables.
+dim(human)
+# The data set contains human development and gender inequality data for
+# different countries and regions (including the entire world). 
+# These indicators can be used to assess development of countries in a number
+# areas, including economic, health, education and equality.
+# The variables are [min, max]:
+# Country: Name of the country (or region)
+# HDI.Rank: Human development index rank [1, 188]
+# HDI: Human development index [0.35, 0.94]
+# Life.Exp: Life expectancy [49, 84]
+# Edu.Exp: Expected years of education [4, 20]
+# Edu.Mean: Mean years of education [1, 13]
+# GNI: Gross national income per capita [581, 123124]
+# GNI.MinusRank: GNI minus HDI rank [-84, 47]
+# GII.Rank: Gender inequality index rank [1, 188]
+# GII: Gender inequality index [0.02, 0.74]
+# Mat.Mor: Maternal mortality ratio [1, 1100]
+# Ado.Birth: Adolescent birth rate [0.6, 204.8]
+# Parli.F: Female representation in parliament (percent) [0, 58]
+# Edu2.F: Population with secondary education (female) [0.9, 100]
+# Edu2.M: Population with secondary education (male) [3, 100]
+# Labo.F: Labour force participation rate (female) [14, 88]
+# Labo.M: Labour force participation rate (female) [44, 96]
+# Edu2.FM: Female/male secondary education ratio [0.2, 1.5]
+# Labo.FM: Female/male labour force participation ratio [0.2, 1.0]
+str(human)
+summary(human)
 
-# human development data set
-# 195 rows, 8 variables
-dim(hd)
-# HDI Rank,
-# Country,
-# Human Development Index (HDI),
-# Life Expectancy at Birth,
-# Expected Years of Education,
-# Mean Years of Education,
-# Gross National Income (GNI) per Capita,
-# GNI per Capita Rank Minus HDI Rank
-str(hd)
-summary(hd)
+# only select these 9 variables
+human <- select(human, c("Country", "Edu2.FM", "Labo.FM", 
+                         "Edu.Exp", "Life.Exp", "GNI", 
+                         "Mat.Mor", "Ado.Birth", "Parli.F"))
 
-# gender inequality data set
-# 195 rows, 12 variables
-dim(gii)
-# GII Rank,
-# Country,
-# Gender Inequality Index (GII),
-# Maternal Mortality Ratio,
-# Adolescent Birth Rate,
-# Percent Representation in Parliament,
-# Population with Secondary Education (Female),
-# Population with Secondary Education (Male),
-# Labour Force Participation Rate (Female),
-# Labour Force Participation Rate (Male)
-str(gii)
-summary(gii)
+# exclude all rows with undefined values
+human <- na.omit(human)
 
-# metadata for variables: https://github.com/KimmoVehkalahti/Helsinki-Open-Data-Science/blob/master/datasets/human_meta.txt
-# rename columns in human development data
-hd <- hd %>% 
-  rename(
-    HDI.Rank = `HDI Rank`,
-    HDI = `Human Development Index (HDI)`,
-    GNI = `Gross National Income (GNI) per Capita`,
-    GNI.MinusRank = `GNI per Capita Rank Minus HDI Rank`,
-    Life.Exp = `Life Expectancy at Birth`,
-    Edu.Exp = `Expected Years of Education`,
-    Edu.Mean = `Mean Years of Education`
-  )
+# remove regions (last 7 rows), leaving just countries
+human <- head(human, -7)
 
-# rename columns in gender inequality data
-gii <- gii %>% 
-  rename(
-    GII.Rank = `GII Rank`,
-    GII = `Gender Inequality Index (GII)`,
-    Mat.Mor = `Maternal Mortality Ratio`,
-    Ado.Birth = `Adolescent Birth Rate`,
-    Parli.F = `Percent Representation in Parliament`,
-    Edu2.F = `Population with Secondary Education (Female)`,
-    Edu2.M = `Population with Secondary Education (Male)`,
-    Labo.F = `Labour Force Participation Rate (Female)`,
-    Labo.M = `Labour Force Participation Rate (Male)`
-  )
+# 155 x 9
+dim(human)
 
-# create new variables in gii data set
-# female to male ratio of secondary education
-gii$Edu2.FM = gii$Edu2.F / gii$Edu2.M
-# female to male ratio of labour force participation
-gii$Labo.FM = gii$Labo.F / gii$Labo.M
-
-# join hd and gii data sets by country (only including those in both)
-human <- inner_join(hd, gii, by = "Country")
-
-# save our new data set to disk
+# save the data set to disk (overwrite)
 write_csv(human, "./data/human.csv")
